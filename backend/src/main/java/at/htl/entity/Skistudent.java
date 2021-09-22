4af58dec27732255a2dd7f20e8ec2718e65dbf7e
primetzvan <vaniprimetz@gmail.com>
2021-09-22 22:51:45 +0200

    added Skistudent entity

---
+++ backend/src/main/java/at/htl/entity/Skistudent.java
@@ -0,0 +1,29 @@
+package at.htl.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Skistudent extends Person{
+
+    public Skistudent(String firstname, String lastname, int age) {
+        super(firstname, lastname, age);
+    }
+
+    protected Skistudent(Long id, String firstname, String lastname, int age) {
+        super(id, firstname, lastname, age);
+    }
+
+    public Skistudent() {
+    }
+
+    @Override
+    public String toString() {
+        return "Person mit der " +
+                "id " + super.getId() + '\'' +
+                ", namens " + super.getFirstname() +
+                " " + super.getLastname() +
+                ", mit dem alter " + super.getAge() +
+                "\n";
+    }
+
+}
