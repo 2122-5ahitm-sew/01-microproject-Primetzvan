31a802cf064fcbac752c2822541e8e2e7b40fbc3
primetzvan <vaniprimetz@gmail.com>
2021-09-22 23:00:46 +0200

    added Booking Entity

---
+++ backend/src/main/java/at/htl/entity/Booking.java
@@ -0,0 +1,64 @@
+package at.htl.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Booking extends PanacheEntity {
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinColumn(name = "Skistudent_ID")
+    private Skistudent student;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinColumn(name = "Course_ID")
+    private Course course;
+
+    public Booking(Skistudent student, Course course) {
+        this.student = student;
+        this.course = course;
+    }
+
+    public Booking(Long id, Skistudent student, Course course) {
+        this.student = student;
+        this.course = course;
+    }
+
+    public Booking() {
+    }
+
+    public Skistudent getStudent() {
+        return student;
+    }
+
+    public void setStudent(Skistudent student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Booking:" +
+                "student=" + student.getLastname() +
+                ", course=" + course.getName();
+    }
+}
