2b3239ee4c8cacb67f7edd25d968fc1b5793e8be
primetzvan <vaniprimetz@gmail.com>
2021-10-13 15:28:01 +0200

    added CourseRepository.java

---
+++ backend/src/main/java/at/htl/repository/CourseRepository.java
@@ -0,0 +1,45 @@
+package at.htl.repository;
+
+import at.htl.entity.Course;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@ApplicationScoped
+@Transactional
+public class CourseRepository implements PanacheRepository<Course> {
+
+  public void save(Course entity){
+
+    if (entity.getId() != null){
+      getEntityManager().merge(entity);
+    }else {
+      persist(entity);
+    }
+  }
+
+  public void delete(long id){
+
+    Course course = findById(id);
+
+    if (course != null){
+      delete(course);
+    }
+
+  }
+
+  public List<Course> findAllCourse(){
+    var query = getEntityManager().createQuery("Select c from Course c", Course.class);
+    return query.getResultList();
+  }
+
+  public Course findById(long id){
+
+    return find("id", id).firstResult();
+
+
+  }
+
+}
