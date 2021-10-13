63c66be9ad1744d4ed3d40067efd4bbbcfc6655f
primetzvan <vaniprimetz@gmail.com>
2021-10-13 15:23:34 +0200

    added SkistudentRepositoryTest.java

---
+++ backend/src/test/java/at/htl/skischool/entity/SkistudentRepositoryTest.java
@@ -0,0 +1,107 @@
+package at.htl.skischool.entity;
+
+import at.htl.entity.Skistudent;
+import at.htl.repository.SkistudentRepository;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@QuarkusTest
+public class SkistudentRepositoryTest {
+
+    @Inject
+    public SkistudentRepository repo;
+
+    @Test
+    void addSkiStudent(){
+
+        Skistudent student = new Skistudent("Hans", "Müller", 10);
+
+        repo.save(student);
+
+        Long id = student.getId();
+
+        assertThat(repo.findById(id).getFirstname()).isEqualTo(student.getFirstname());
+
+    }
+
+    @Test
+    void updateSkiStudent(){
+
+        Long id;
+
+        Skistudent student = new Skistudent("Hans", "Müller", 10);
+
+        repo.save(student);
+
+        id = student.getId();
+        Skistudent studentnew = new Skistudent(id,"Hans", "Müller", 11);
+
+        repo.save(studentnew);
+
+        id = student.getId();
+
+        assertThat(repo.findById(id).getFirstname()).isEqualTo(student.getFirstname());
+
+    }
+
+    @Test
+    void deleteSkiStudent(){
+
+        Skistudent student = new Skistudent("Hans", "Müller", 10);
+
+        repo.save(student);
+
+        Long id = student.getId();
+
+        repo.delete(id);
+
+      assertThat(repo.findById(id)).isNull();
+
+    }
+
+    @Test
+    void findAll(){
+
+        List<Skistudent> list;
+
+        Skistudent student = new Skistudent("Hans", "Müller", 10);
+        Skistudent student1 = new Skistudent("Peter", "Hofer", 50);
+        Skistudent student2 = new Skistudent("Lisa", "Müller", 25);
+
+        repo.save(student);
+        repo.save(student1);
+        repo.save(student2);
+
+        list = repo.findAll().list();
+
+        assertThat(list).hasSize(3);
+    }
+
+    @Test
+    void findById(){
+
+        Long id;
+        Skistudent s;
+
+        Skistudent student = new Skistudent("Hans", "Müller", 10);
+        Skistudent student1 = new Skistudent("Peter", "Hofer", 50);
+        Skistudent student2 = new Skistudent("Lisa", "Müller", 25);
+
+        repo.save(student);
+        repo.save(student1);
+        repo.save(student2);
+
+        id = student.getId();
+
+        s = repo.findById(id);
+
+        assertThat(s.getId()).isEqualTo(student.getId());
+
+    }
+
+}
